c43b4677d63aeb031f6c096fcab9a6b562fc841c
ashoaib <abu.rm.shoaib@gmail.com>
2013-05-04 20:53:17 +0100

    Added URL schema check

--- site_monitor.py
+++ site_monitor.py
@@ -3,12 +3,41 @@ import sys
 
 
 class SiteMonitor:
-    def __init__(self, url):
+    """
+    This is a simple wrapper class around the requests module. It can be used
+    to monitor the HTTP status code returned by a website or URL. If the status
+    code doesn't match an expected value, it can be used to send an email with
+    the status.
+    """
+
+    def __init__(self, url=None):
+        self.url = None
+
+        if url is not None:
+            self.set_url(url)
+
+    def set_url(self, url):
+        if len(url.split(':')) < 2:
+            url = 'http://' + url
+
         self.url = url
-        self.request = requests.get(url)
-        self.status_code = self.request.status_code
+
+    def make_request(self):
+        if self.url is not None:
+            self.request = requests.get(self.url)
+        else:
+            raise AttributeError("URL is not set or is invalid")
+
+    @property
+    def status_code(self):
+        return self.request.status_code
 
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    sm = SiteMonitor(url)
+
+    sm = SiteMonitor()
+    sm.set_url(url)
+    sm.make_request()
+
+    print sm.status_code
